1d3cc1f7bd0079738350eccae809e1eb4513f976
susobhankr <susobhan3114@gmail.com>
2022-02-08 23:35:32 +0530

    DiceRolled

--- src/Com/BridgeLabz/dice1.java
+++ src/Com/BridgeLabz/dice1.java
@@ -3,11 +3,12 @@ package Com.BridgeLabz;
 public class dice1 {
 
 	public static void main(String[] args) {
-		int startPosition = 0;
-
-		System.out.println("Starting position of Snake and Ladder is : " + startPosition);
-
-
+		// TODO Auto-generated method stub
+				double empCheck;
+		        
+				empCheck=Math.floor((Math.random()*6)+1);
+				int dice=(int)empCheck;
+				System.out.println("Dice number= "+dice);
 	}
 
 }
