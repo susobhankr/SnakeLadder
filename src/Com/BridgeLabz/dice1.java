db3981c5381c70524e5c3e9e5e172d57c26a8d2d
susobhankr <susobhan3114@gmail.com>
2022-02-09 00:14:19 +0530

    Till100

--- src/Com/BridgeLabz/dice1.java
+++ src/Com/BridgeLabz/dice1.java
@@ -3,8 +3,7 @@ package Com.BridgeLabz;
 import java.util.Random; 
 
 public class dice1 {
-		
-		//class or static variable
+	//class or static variable
 		static final int Snake = 2;
 		static final int ladder = 1;
 		static final int Noplay = 0;
@@ -12,14 +11,16 @@ public class dice1 {
 		public static void main(String[] args) 
 		{
 			int position = 0;
+			while (position < 100) 
+			{
 			Random ran = new Random();
 			int dice = ran.nextInt(6)+1;
-			System.out.println("Dice: "+dice);
+			System.out.println("dice: "+dice);
 			
 			int optionCheck = ran.nextInt(3);
 			System.out.println("optionCheck: "+optionCheck);
 			
-			if (optionCheck == ladder) 
+			if ((optionCheck == ladder) && (position + dice)<=100)
 			{
 				System.out.println("Ladder");
 				position = position + dice;
@@ -35,7 +36,7 @@ public class dice1 {
 			}
 			if (position<0)
 				position=0;
-			System.out.println("Position: "+position);
-	}
-
+			System.out.println("position: "+position);
+			}
+		}
 }
