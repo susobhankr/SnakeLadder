dee2eb5958b047b4a02f1e45105479a1d9012c88
susobhankr <susobhan3114@gmail.com>
2022-02-09 00:05:25 +0530

    NoPlay

--- src/Com/BridgeLabz/dice1.java
+++ src/Com/BridgeLabz/dice1.java
@@ -1,14 +1,41 @@
 package Com.BridgeLabz;
 
-public class dice1 {
+import java.util.Random; 
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-				double empCheck;
-		        
-				empCheck=Math.floor((Math.random()*6)+1);
-				int dice=(int)empCheck;
-				System.out.println("Dice number= "+dice);
+public class dice1 {
+		
+		//class or static variable
+		static final int Snake = 2;
+		static final int ladder = 1;
+		static final int Noplay = 0;
+
+		public static void main(String[] args) 
+		{
+			int position = 0;
+			Random ran = new Random();
+			int dice = ran.nextInt(6)+1;
+			System.out.println("Dice: "+dice);
+			
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			if (optionCheck == ladder) 
+			{
+				System.out.println("Ladder");
+				position = position + dice;
+			}
+			
+			else if (optionCheck == Snake) 
+			{
+				System.out.println("Snake");
+				position = position - dice;
+			}
+			else {
+				System.out.println("No Play");
+			}
+			if (position<0)
+				position=0;
+			System.out.println("Position: "+position);
 	}
 
 }
