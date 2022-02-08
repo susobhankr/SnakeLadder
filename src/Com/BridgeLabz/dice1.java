6e2fb0451ee257e3cd775fed6111403b0b569c9e
susobhankr <susobhan3114@gmail.com>
2022-02-09 00:25:18 +0530

    Reached100

--- src/Com/BridgeLabz/dice1.java
+++ src/Com/BridgeLabz/dice1.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class dice1 {
 		
-		//class or static variable
+	//class or static variable
 		static final int Snake = 2;
 		static final int ladder = 1;
 		static final int Noplay = 0;
@@ -12,14 +12,16 @@ public class dice1 {
 		public static void main(String[] args) 
 		{
 			int position = 0;
+			while (position < 100) 
+			{
 			Random ran = new Random();
 			int dice = ran.nextInt(6)+1;
 			System.out.println("Dice: "+dice);
 			
 			int optionCheck = ran.nextInt(3);
-			System.out.println("optionCheck: "+optionCheck);
+			System.out.println("OptionCheck: "+optionCheck);
 			
-			if (optionCheck == ladder) 
+			if ((optionCheck == ladder) && (position + dice)<=100)
 			{
 				System.out.println("Ladder");
 				position = position + dice;
@@ -36,6 +38,6 @@ public class dice1 {
 			if (position<0)
 				position=0;
 			System.out.println("Position: "+position);
-	}
-
+			}
+		}
 }
